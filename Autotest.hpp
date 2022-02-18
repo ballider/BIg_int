6129f90a4bcb4b0658f08892b9a3485d97e8061b
ballider <90126551+ballider@users.noreply.github.com>
2022-02-18 11:10:13 +0300

    Delete Autotest.hpp

--- Autotest.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef A25AF939_EE99_4FAD_9393_F578A7ABDEF9
-#define A25AF939_EE99_4FAD_9393_F578A7ABDEF9
-
-bool AutoTest1();
-bool AutoTest2();
-bool AutoTest3();
-bool AutoTest();
-
-#endif /* A25AF939_EE99_4FAD_9393_F578A7ABDEF9 */
